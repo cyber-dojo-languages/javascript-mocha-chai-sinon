2186bf571327e5558c7e0b29db4f33cfac0880d6
Jon Jagger <jon@jaggersoft.com>
2018-03-31 09:21:39 +0100

    upgrade to node 9.10.1; only run if jshint passes

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,6 +1,10 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
+  error_pattern = /(\d+) errors?/
+  if error_pattern.match(output)
+    return :amber
+  end
   return :red   if /AssertionError:/.match(output)
   return :amber if /SyntaxError:/.match(output)
   return :amber if /ReferenceError:/.match(output)
