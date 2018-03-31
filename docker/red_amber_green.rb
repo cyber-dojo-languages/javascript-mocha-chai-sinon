93a88ed2ebc2236e689be711620ba3083608bc9b
Jon Jagger <jon@jaggersoft.com>
2018-03-31 11:11:23 +0100

    refactoring; improve red-amber-green regex

--- docker/red_amber_green.rb
+++ docker/red_amber_green.rb
@@ -1,10 +1,8 @@
 
 lambda { |stdout,stderr,status|
   output = stdout + stderr
-  error_pattern = /(\d+) errors?/
-  if error_pattern.match(output)
-    return :amber
-  end
+  js_hint_pattern = /^(\d+) error(s?)/
+  return :amber if js_hint_pattern.match(output)
   return :red   if /AssertionError:/.match(output)
   return :amber if /SyntaxError:/.match(output)
   return :amber if /ReferenceError:/.match(output)
